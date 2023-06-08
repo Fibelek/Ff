d311557f5e76b6e2e213666ee39ad0173455e304
Fibelek <131624031+Fibelek@users.noreply.github.com>
2023-06-08 07:56:31 +0300

    Add files via upload

---
+++ 7pr/Program.cs
@@ -0,0 +1,26 @@
+﻿// Методы 
+
+static int CountWordsEndingWithChar(string inputString, char endingChar)
+{
+    int count = 0;
+    string[] words = inputString.Split(' ');
+
+    foreach (string word in words)
+    {
+        if (word.EndsWith(endingChar.ToString()))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+Console.WriteLine("Введите слова");
+string inputString = Console.ReadLine();
+Console.WriteLine("Введите последний символ слова");
+char endingChar = char.Parse(Console.ReadLine());
+
+int count = CountWordsEndingWithChar(inputString, endingChar);
+
+Console.WriteLine($"слов которые кончаются на эту буква '{endingChar}': {count}");
+
